e42648f37436ce68fb2feb4a253c04bc2a62da63
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-28 18:22:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  reverse_array = []
-  sentence_array.collect {|word| reverse_array << word.reverse}
-  reverse_array.join(" ")
+  sentence_array.collect! {|word| word.reverse}
+  sentence_array.join(" ")
 end
